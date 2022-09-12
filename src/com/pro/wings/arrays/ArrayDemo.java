abe11568298185abca5bace846858d23fb7aa8b8
JD <jayprowings@gmail.com>
2022-09-12 08:19:47 +0530

    first commit

---
+++ src/com/pro/wings/arrays/ArrayDemo.java
@@ -0,0 +1,17 @@
+package com.pro.wings.arrays;
+
+public class ArrayDemo {
+	
+	public static void main(String[] args) {
+		
+		int[] numbers = {10,20,30};
+		
+		for(int n : numbers)
+		{
+			System.out.println(n);
+		}
+		
+		
+	}
+
+}
