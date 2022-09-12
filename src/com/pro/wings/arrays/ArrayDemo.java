2e62a1c6e295b3e568b09a5cd52141963448b221
JD <jayprowings@gmail.com>
2022-09-12 08:29:31 +0530

    replaced with normal for loop

--- src/com/pro/wings/arrays/ArrayDemo.java
+++ src/com/pro/wings/arrays/ArrayDemo.java
@@ -6,9 +6,9 @@ public class ArrayDemo {
 		
 		int[] numbers = {10,20,30};
 		
-		for(int n : numbers)
+		for(int i=0; i<numbers.length; i++)
 		{
-			System.out.println(n);
+			System.out.println(numbers[i]);
 		}
 		
 		
