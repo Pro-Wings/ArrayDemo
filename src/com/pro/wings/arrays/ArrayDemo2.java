21fe56f89ce60fb8de8b7c3cce847476e7f08b88
JD <jayprowings@gmail.com>
2022-09-12 08:26:44 +0530

    added another demo class

---
+++ src/com/pro/wings/arrays/ArrayDemo2.java
@@ -0,0 +1,16 @@
+package com.pro.wings.arrays;
+
+import java.util.Arrays;
+
+public class ArrayDemo2 {
+	
+	public static void main(String[] args) {
+		
+		int[] numbers = {10,20,30};
+		
+		System.out.println(Arrays.toString(numbers));
+		
+		
+	}
+
+}
